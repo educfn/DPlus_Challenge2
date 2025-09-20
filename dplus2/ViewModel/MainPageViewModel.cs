1cb736a666d434611a12e4872f999c24d921aec7
Eduardo CFN <eduardo.cardoso10@gmail.com>
2025-09-20 11:54:52 -0300

    chore [Create Viewmodel] - Created the view model for the MainPage with properties which represents the data the user will set in the MainPage. - The viewmodel will also be moved to a directory which represents the viewmodels.

---
+++ dplus2/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dplus2.ViewModel
+{
+    public class MainPageViewModel
+    {
+        public int PrecoInicial { get; set; }
+        public int Volatilidade { get; set; }
+        public int MediaRetorno { get; set; }
+        public int Tempo { get; set; }
+    }
+}
