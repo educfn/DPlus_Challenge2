0b131c151d7a3b34a34b6059df3ad7ec4a52144f
Eduardo CFN <eduardo.cardoso10@gmail.com>
2025-09-20 10:05:12 -0300

    fresh project created commit

---
+++ dplus2/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace dplus2;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ dplus2/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "dplus2")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "dplus2.Pages")]
