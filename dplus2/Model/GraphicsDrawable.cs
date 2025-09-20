a27796648323e02a150e343d2898b47bfcdf64dd
Eduardo CFN <eduardo.cardoso10@gmail.com>
2025-09-20 16:48:19 -0300

    feat [GraphicView]: - Implemented the GraphicsView with the IDrawable class "GraphicsDrawable". - There is some code implemented in the GraphicsDrawable.Draw method, to show that the GraphicsView is working and is bound to the GraphicsDrawable class.

---
+++ dplus2/Model/GraphicsDrawable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dplus2.Model
+{
+    public class GraphicsDrawable : IDrawable
+    {
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            //Test Graphics Code
+            PathF path = new PathF();
+            path.MoveTo(40, 10);
+            path.LineTo(70, 80);
+            path.LineTo(10, 50);
+            path.Close();
+            canvas.StrokeColor = Colors.Green;
+            canvas.StrokeSize = 6;
+            canvas.DrawPath(path);
+        }
+    }
+}
