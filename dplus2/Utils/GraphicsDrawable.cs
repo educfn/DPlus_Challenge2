66ca0466eed4cb5c909cacf5857ba85e8226f099
Eduardo CFN <eduardo.cardoso10@gmail.com>
2025-09-21 11:35:17 -0300

    feat [GraphicsDrawable improvement] - Modified the Draw method to draw lines, which are better then Path, and filled circles to point out the value point in the graphics.

--- dplus2/Utils/GraphicsDrawable.cs
+++ dplus2/Utils/GraphicsDrawable.cs
@@ -2,26 +2,27 @@
 {
     public class GraphicsDrawable : IDrawable
     {
-        private double[] y_points = Array.Empty<double>();
+        private float[] y_points = Array.Empty<float>();
 
         public void Draw(ICanvas canvas, RectF dirtyRect)
         {
             if (y_points.Length == 0) return;
 
-            PathF path = new PathF();
-            float x_point = 0.0f;
-            
-            foreach(double y_point in y_points)
-                path.LineTo(x_point++, (float)y_point);
-
-            canvas.StrokeColor = Colors.Green;
+            canvas.StrokeColor = canvas.FillColor = Colors.Green;
             canvas.StrokeSize = 6;
-            canvas.DrawPath(path);
+            float last_xpoint = dirtyRect.X;
+            
+            for (int y_index = 0; y_index < y_points.Length - 1; )
+            {
+                var valor = y_points[y_index];
+                canvas.FillCircle(last_xpoint, y_points[y_index], 6);
+                canvas.DrawLine(last_xpoint, y_points[y_index],last_xpoint += 20, y_points[++y_index]);
+            }
         }
 
         public void AddY_Points(double[] y_points)
         {
-            this.y_points = y_points;
+            this.y_points = y_points.ToList().Select(d => (float)d).ToArray();
         }
     }
 }
