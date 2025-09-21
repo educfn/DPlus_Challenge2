673479e4913e0b27dc7cdfc0bbea354c18c14e43
Eduardo CFN <eduardo.cardoso10@gmail.com>
2025-09-21 20:24:43 -0300

    feat [Multi line implementation] - Implementing a multi line simulation.

--- dplus2/ViewModel/MainPageViewModel.cs
+++ dplus2/ViewModel/MainPageViewModel.cs
@@ -20,17 +20,25 @@ namespace dplus2.ViewModel
 
         public ICommand BtnIniciarSimulacao => ComandoIniciarSimulacao;
         public GraphicsDrawable GraphicsDrawable { get; set; }
-        public Action? RedrawGraphicsView { get; set;}
+        public Action? RedrawGraphicsView { get; set; }
         public float PrecoInicial { get; set; }
         public float Volatilidade { get; set; }
         public float MediaRetorno { get; set; }
+        public int QuantidadeDeSimulacoes { get; set; }
         public int Tempo { get; set; }
 
         private void IniciarSimulacao()
         {
             if (Tempo <= 0 || RedrawGraphicsView == null) return;
 
-            GraphicsDrawable.AddY_Points(BrownianMotion.GenerateBrownianMotion(Volatilidade, MediaRetorno, PrecoInicial, Tempo));
+            double[][] matrix_ypoints = new double[QuantidadeDeSimulacoes][];
+
+            for (int i = 0; i < QuantidadeDeSimulacoes; i++)
+            {
+                matrix_ypoints[i] = BrownianMotion.GenerateBrownianMotion(Volatilidade, MediaRetorno, PrecoInicial, Tempo);
+            }
+
+            GraphicsDrawable.Add_YPoints_And_NSimulation(matrix_ypoints, QuantidadeDeSimulacoes);
             RedrawGraphicsView.Invoke();
         }
     }
